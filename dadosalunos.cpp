2ab1dee4c8c307802629d18f90db6caf5bae0d89
Guilherme Ryan <162244223+GuilhermeRyanz@users.noreply.github.com>
2024-04-05 20:25:31 -0400

    novos arquivos
    os aquivos la

---
+++ dadosalunos.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int idade = 0;
+    float altura = 0;
+
+    cout << "digite sua idade " << endl;
+    cin >> idade;
+    cout << "digite seu tamanho " << endl;
+    cin >> altura;
+
+    cout << "A sua idade é " << idade << "Seu tamanho é " << altura << endl;
+
+    return 0;
+
+
+
+
+
+}
---
+++ teste.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+    int a = 50;
+    int b = 40;
+
+    int soma = a+b;
+
+    cout << soma << endl;
+
+    return 0;
+
+}
