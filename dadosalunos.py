a6f43ac342a5b8e253e53d28bcc2f35f6d4fd815
Guilherme Ryan <guilherme.ryan.etech@gmail.com>
2024-04-05 20:40:24 -0400

    firt commit

---
+++ dadosalunos.py
@@ -0,0 +1,5 @@
+nome = str(input("Digite seu nome: "))
+idade = int(input("Digite sua idade: "))
+altura = float(input("Digite sua altura: "))
+
+print(f"Seu nome é {nome} sua altura é {altura} e sua idade é {idade}")
