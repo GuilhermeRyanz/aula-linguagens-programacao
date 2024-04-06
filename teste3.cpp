3c5af10fd684d6e21593ce4c0d66cf9858defa0b
Guilherme Ryan <162244223+GuilhermeRyanz@users.noreply.github.com>
2024-04-05 20:29:24 -0400

    teste numero 3 do git

---
+++ teste3.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+ int main (){
+    int idade = 0;
+
+    cout << "digite sua idade " << endl;
+    cin >> idade;
+
+    cout << "A sua idade eh " << idade << endl;
+
+
+    return 0;
+    }
